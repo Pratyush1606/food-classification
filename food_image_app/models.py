7848013f47fdba70bdd23c82258797019692dd99
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2022-03-24 16:26:06 +0530

    Added image model and serializers

--- food_image_app/models.py
+++ food_image_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Kid(models.Model):
@@ -7,3 +8,21 @@ class Kid(models.Model):
     kid_age = models.IntegerField()
     parent_phone_number = models.CharField(max_length=100)
     parent_email_address = models.EmailField(max_length=100)
+
+class FoodGroup(models.TextChoices):
+    # Refer here for enum https://docs.djangoproject.com/en/3.2/ref/models/fields/#field-choices-enum-types
+    Fruit = 'Fruit', _('Fruit')
+    Vegetable = 'Vegetable', _('Vegetable')
+    Grain = 'Grain', _('Grain')
+    Dairy = 'Dairy', _('Dairy')
+    Confectionery = 'Confectionery', _('Confectionery')
+    Unknown = 'Unknown', _('Unknown')
+
+class Image(models.Model):
+    image_id = models.AutoField(primary_key=True, editable=False)
+    kid = models.ForeignKey(to=Kid, on_delete=models.CASCADE, related_name="kid_food_images")
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    is_approved = models.BooleanField(default=False)
+    approved_by = models.CharField(max_length=100)
+    food_group = models.CharField(max_length=100, choices=FoodGroup.choices)
--- food_image_app/serializers.py
+++ food_image_app/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Kid
+from .models import Kid, Image
 
 class KidSerializer(serializers.ModelSerializer):
     class Meta:
         model = Kid
         fields = ['kid_id', 'kid_name', 'kid_age', 'parent_phone_number', 'parent_email_address']
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ['image_id', 'kid', 'created_on', 'updated_on', 'is_approved', 'approved_by', 'food_group']
