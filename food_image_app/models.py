3a3156e156f61db3e7bd1b45391116fe5ba5a5dd
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2022-03-24 15:29:45 +0530

    Added Kid model

--- food_image_app/models.py
+++ food_image_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Kid(models.Model):
+    kid_id = models.AutoField(primary_key=True, editable=False)
+    kid_name = models.CharField(max_length=100)
+    kid_age = models.IntegerField()
+    parent_phone_number = models.CharField(max_length=100)
+    parent_email_address = models.EmailField(max_length=100)
